111ac159934fce0095c88c5231927dd4d9e00ade
sevak <sevak.gharibian@gmail.com>
2015-10-24 14:58:21 +0330

    added mvc unit test, removed mvc integration test

--- src/test/java/com/infotech/ivr/reporting/web/controller/ProductTest.java
+++ src/test/java/com/infotech/ivr/reporting/web/controller/ProductTest.java
@@ -1,6 +1,10 @@
-package com.infotech.ivr.reporting.it;
+package com.infotech.ivr.reporting.web.controller;
 
 import com.infotech.ivr.reporting.domain.Product;
+import com.infotech.ivr.reporting.service.ProductService;
+
+import java.util.Arrays;
+import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,12 +14,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyInt;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
@@ -24,30 +34,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * integration test for product web.
+ * unit test for product web.
  *
  * @author Sevak Gahribian
  */
 @WebAppConfiguration
-@ContextConfiguration(locations = { "classpath:spring/applicationContext.xml", "classpath:spring/applicationContext-mvc.xml" })
-public class ProductIT extends AbstractTestNGSpringContextTests  {
+@ContextConfiguration(locations = { "classpath:spring/applicationContext-mvc.xml", 
+                                    "classpath:spring/testContext.xml",  })
+public class ProductTest extends AbstractTestNGSpringContextTests  {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ProductIT.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ProductTest.class);
 
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
 
-    @BeforeClass
+    @Autowired
+    private ProductService productService;
+
+    @BeforeMethod(alwaysRun = true)
     public void setup() {
+        Mockito.reset(productService);
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
     public void getProductsList() throws Exception {
+        Product p1 = new Product();
+        p1.setName("book");
+        p1.setPrice(new BigDecimal(215.33));
+        Product p2 = new Product();
+        p2.setName("guitar");
+        p2.setPrice(new BigDecimal(1500.00));
+        when(productService.findAllPageable(anyInt(), anyInt())).thenReturn(Arrays.asList(p1, p2));
+        long count = 2;
+        when(productService.getCount()).thenReturn(count);
         mockMvc.perform(get("/products"))
-            .andExpect(status().isOk());
-    }
+            .andExpect(status().isOk())
+            .andDo(log());
+   }
 }
 
