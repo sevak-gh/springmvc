4fabba5dda82b5211acb5efcafc14cfd59d01db2
sevak gharibian <sevak.gharibian@gmail.com>
2015-12-20 11:35:49 +0330

    added interceptor and aspect for response time log

---
+++ src/main/java/com/infotech/ivr/reporting/util/MethodLogger.java
@@ -0,0 +1,34 @@
+package com.infotech.ivr.reporting.util;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Around;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * generic method entry logger.
+ *
+ * @author Sevak Gharibian
+ */
+@Aspect
+@Component
+public class MethodLogger {
+
+    private static final Logger LOG = LoggerFactory.getLogger("profiler");
+
+    // around execution any public method
+    @Around("execution(public * com.infotech.ivr.reporting.service..*.*(..))"
+            + " || execution(public * com.infotech.ivr.reporting.repository..*.*(..))")
+    public Object logMethodEntry(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        LOG.debug("{}.{}({}): {} in {} msec", joinPoint.getSignature().getDeclaringType().getSimpleName(),
+                  joinPoint.getSignature().getName(),
+                  joinPoint.getArgs(),
+                  result,
+                  System.currentTimeMillis() - start);
+        return result;
+    }
+}
---
+++ src/main/java/com/infotech/ivr/reporting/web/interceptor/RequestTimeLogger.java
@@ -0,0 +1,55 @@
+package com.infotech.ivr.reporting.web.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * request time logger.
+ *
+ * @author Sevak Gharibian
+ *
+ */
+@Component
+public class RequestTimeLogger extends HandlerInterceptorAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger("profiler");
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request, 
+                             HttpServletResponse response, 
+                             Object handler) throws Exception {
+        long time = System.currentTimeMillis();
+        request.setAttribute("startTime", time);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, 
+                           HttpServletResponse response, 
+                           Object handler, 
+                           ModelAndView modelAndView) throws Exception {
+        long time = System.currentTimeMillis();
+        request.setAttribute("startViewTime", time);
+   }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception exceptionIfAny) throws Exception {
+        long time = System.currentTimeMillis();
+        long startTime = (long)request.getAttribute("startTime");
+        long startViewTime = (long)request.getAttribute("startViewTime");
+        LOG.debug("{} --> total:{}, start-view:{}, view-end:{}", 
+                    request.getRequestURI(), 
+                    time-startTime, 
+                    startViewTime - startTime,
+                    time - startViewTime);
+    }
+}
